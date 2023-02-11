46279ef53c2c9bd4dad41b5275d7244770a87906
David Sampson <davidcsampson@gmail.com>
2023-02-11 16:36:40 -0500

    Update shuffle.py

--- shuffle.py
+++ shuffle.py
@@ -1,6 +1,6 @@
 import random
 #This code was written by ChatGPT
-def shuffle_and_deal(num_jokers, num_players):
+def shuffle_and_deal(num_players, num_jokers = 2):
     deck = ['Ace of Spades', '2 of Spades', '3 of Spades', '4 of Spades', '5 of Spades', '6 of Spades', '7 of Spades',
             '8 of Spades', '9 of Spades', '10 of Spades', 'Jack of Spades', 'Queen of Spades', 'King of Spades',
             'Ace of Hearts', '2 of Hearts', '3 of Hearts', '4 of Hearts', '5 of Hearts', '6 of Hearts', '7 of Hearts',
@@ -26,7 +26,7 @@ def shuffle_and_deal(num_jokers, num_players):
 
 
 # if __name__ == '__main__':
-#     hands, leftover = shuffle_and_deal(2,55)
+#     hands, leftover = shuffle_and_deal(3)
 #     for hand in hands:
 #         print(f"{hand} \n{len(hand)}")
 #     print(f"Leftover: {leftover}")
