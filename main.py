6919d0d1a437926eacae505d5214eab52354d3d8
rhit-chanb <chan992599@gmail.com>
2023-02-11 00:01:44 -0500

    hidden command prototype works

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+# class MyClient(discord.Client):
+#     async def on_ready(self):
+#         print(f'Logged on as {self.user}!')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(os.environ['TOKEN'])
+
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def sync(ctx):
+    synced = await ctx.bot.tree.sync()
+
+@bot.hybrid_command()
+async def ping(ctx):
+    await ctx.interaction.response.send_message(content = 'pong', ephemeral = True)
+    # await ctx.send(content = 'pong', ephemeral = True)
+
+# client = MyClient(intents=intents)
+# client.run(os.environ['TOKEN'])
+bot.run(os.environ['TOKEN'])
