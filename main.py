d26ab65005a04a1ec307ce4d5033e693aa2087ff
David Sampson <davidcsampson@gmail.com>
2023-02-11 01:42:55 -0500

    Some boilerplate

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 player_list = []
+game_in_progress = False
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
@@ -25,32 +26,75 @@ async def ping(ctx):
     await ctx.interaction.response.send_message(content = 'pong', ephemeral = True)
 
 @bot.hybrid_command()
-async def join(ctx):
-    print('join command run by user: ')
-    print(ctx.message.author.name)
-    new_player = ctx.message.author.name
-    if new_player in player_list:
-        await ctx.interaction.response.send_message(content = "You are already in the game!")
+async def start(ctx):
+    global game_in_progress
+    if not game_in_progress:
+        game_in_progress = True
+        await respond_global(ctx=ctx,response="THE GAME HAS STARTED") #TODO: Print out the standings
         return
-    player_list.append(new_player)
-    await ctx.interaction.response.send_message(content = 'You have joined the ongoing game as ' + new_player, ephemeral = True)
+    await respond_ghost(ctx=ctx,response="Game in progress dummy, don't jump the gun")
+
+@bot.hybrid_command()
+async def screwthis(ctx):
+    global game_in_progress
+    global player_list
+    player = ctx.message.author.name
+    if game_in_progress:
+        game_in_progress = False
+        player_list = []
+        await respond_global(ctx = ctx, response=f"Player {player} flipped the table. Laugh at them!")
+        return
+    await respond_ghost(ctx = ctx, response="Game hasn't started dummy don't try and quit already")
+
+
+@bot.hybrid_command()
+async def join(ctx):
+    new_player = ctx.message.author.name
+    print(f'join command run by user: {new_player}')
+    if not game_in_progress:
+        if new_player in player_list:
+            await respond_ghost(ctx = ctx, response = "You are already in the game!")
+            return
+        player_list.append(new_player)
+        await respond_ghost(ctx = ctx, response = f'You have joined the ongoing game as {new_player}')
+        return
+    await respond_ghost(ctx=ctx,response="Can't join a game in progress")
+
 
 @bot.hybrid_command()
 async def leave(ctx):
-    player = ctx.message.author.name
-    if player in player_list:
-        print(f'Leave command run by user: {player}')
-        player_list.remove(player)
-        await respond(ctx, 'You have left the ongoing game')
+    if not game_in_progress:
+        if (player := ctx.message.author.name) in player_list:
+            print(f'Leave command run by user: {player}')
+            player_list.remove(player)
+            await respond_ghost(ctx = ctx, response = 'You have left the ongoing game')
+            return
+        await respond_ghost(ctx = ctx, response = "You aren't in the current game! use /join to join")
         return
-    await respond(ctx, "You aren't in the current game!")
+    await respond_ghost(ctx=ctx,response="Can't leave mid match, find the secret command to end the game!")
+
+@bot.hybrid_command()
+async def rmplayer(ctx, player_name):
+    print(f"To Remove {player_name}")
+    if not game_in_progress:
+        if player_name in player_list:
+            player_list.remove(player_name)
+            await respond_global(ctx=ctx,response=f"Player: {player_name} has been removed from the game")
+            return
+        await respond_ghost(ctx=ctx,response=f"Player: {player_name} is currently not in the game")
+        return
+    await respond_ghost(ctx=ctx, response="Cannot remove a player mid game")
+
 
 @bot.hybrid_command()
 async def listp(ctx):
     print(player_list)
-    await ctx.interaction.response.send_message(content = player_list)
+    await respond_global(ctx = ctx, response = player_list)
 
-async def respond(ctx, response):
+async def respond_ghost(ctx, response):
     await ctx.interaction.response.send_message(content=response, ephemeral=True)
+
+async def respond_global(ctx, response):
+    await ctx.interaction.response.send_message(content=response)
 
 bot.run(os.environ['TOKEN'])
